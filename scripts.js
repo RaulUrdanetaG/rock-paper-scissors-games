24a31b834df4ac5170fa0c8a0e3e5b2bf4fa7020
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2023-07-18 14:40:30 -0500

    Created whole UI

--- scripts.js
+++ scripts.js
@@ -3,15 +3,15 @@ let playerLives = 5;
 let computerLives = 5;
 let round = 0
 
-function getComputerChoice(){
-    const weapons = ['Rock','Paper','Scissors']
+function getComputerChoice() {
+    const weapons = ['Rock', 'Paper', 'Scissors']
     const computerSelection = weapons[Math.floor(Math.random() * weapons.length)]
 
     return computerSelection
 }
 
-function playRound(playerSelection,computerSelection){
-    switch(true){
+function playRound(playerSelection, computerSelection) {
+    switch (true) {
         case (playerSelection === computerSelection):
             console.log(`Thats a draw!, a pair of ${computerSelection}s means a draw`);
             break;
@@ -19,42 +19,42 @@ function playRound(playerSelection,computerSelection){
         case (playerSelection === 'Paper' && computerSelection === 'Rock'):
         case (playerSelection === 'Scissors' && computerSelection === 'Paper'):
             console.log(`You beat that computer!, ${playerSelection} beats ${computerSelection}`);
-            computerLives -=1;
+            computerLives -= 1;
             break;
         case (playerSelection === 'Rock' && computerSelection === 'Paper'):
         case (playerSelection === 'Paper' && computerSelection === 'Scissors'):
         case (playerSelection === 'Scissors' && computerSelection === 'Rock'):
             console.log(`You lost!, ${playerSelection} beats ${computerSelection}`);
-            playerLives-=1;
+            playerLives -= 1;
             break;
     }
     console.log(playerLives);
     console.log(computerLives);
-    return [playerLives,computerLives];
+    return [playerLives, computerLives];
 }
 
-function countRounds(){
+function countRounds() {
     round += 1;
     console.log(round);
     return round;
 }
 
-function resetGame(){
+function resetGame() {
     playerLives = 5;
     computerLives = 5;
     round = 0;
 }
 
-function endGame(playerLives,computerLives){
-    if(playerLives === 0 || computerLives === 0){
+function endGame(playerLives, computerLives) {
+    if (playerLives === 0 || computerLives === 0) {
         console.log("Game has ended")
         resetGame();
     }
 }
 
-function playGame(){
+function playGame() {
     let playerSelection = prompt('Select your weapon (Rock, Paper, Scissors)');
-    playRound(playerSelection,getComputerChoice());
+    playRound(playerSelection, getComputerChoice());
     countRounds();
-    endGame(playerLives,computerLives);
+    endGame(playerLives, computerLives);
 }
