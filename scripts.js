ddb290ccc9be5837f430e8839d657aa303c704c1
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2023-07-18 17:59:39 -0500

    Added functionality to the player buttons

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
-const weaponButtons = document.querySelectorAll('.weapon-button');
+const weaponButtons = document.querySelectorAll('.player-button');
+const playerImg = document.querySelectorAll('.player-button img');
+const computerImg = document.querySelectorAll('.computer-weapon');
 const rounds = document.querySelector('.rounds .number');
 const cpuWins = document.querySelector('.computer-wins .number');
 const playWins = document.querySelector('.player-wins .number');
 const gameInfo = document.querySelector('.round-info');
 const playAgainButton = document.getElementById('play-again-button');
-const computerImg = document.querySelectorAll('.computer-weapon')
 
 let playerWins = 0;
 let computerWins = 0;
@@ -17,18 +18,27 @@ function getComputerChoice() {
     switch (computerSelection){
         case 'Rock':
             computerImg[0].style.filter = 'none';
+            computerImg[0].style.transform = 'scale(1.1)';
             computerImg[1].style.filter = 'grayscale(100%)';
+            computerImg[1].style.transform = 'scale(1)';
             computerImg[2].style.filter = 'grayscale(100%)';
+            computerImg[2].style.transform = 'scale(1)';
             break;
         case 'Paper':
             computerImg[0].style.filter = 'grayscale(100%)';
+            computerImg[0].style.transform = 'scale(1)';
             computerImg[1].style.filter = 'none';
+            computerImg[1].style.transform = 'scale(1.1)';
             computerImg[2].style.filter = 'grayscale(100%)';
+            computerImg[2].style.transform = 'scale(1)';
             break;
         case 'Scissors':
             computerImg[0].style.filter = 'grayscale(100%)';
+            computerImg[0].style.transform = 'scale(1)';
             computerImg[1].style.filter = 'grayscale(100%)';
+            computerImg[1].style.transform = 'scale(1)';
             computerImg[2].style.filter = 'none';
+            computerImg[2].style.transform = 'scale(1.1)';
             break;
     }
     return computerSelection
@@ -73,24 +83,87 @@ function resetGame() {
     playWins.innerText = `${playerWins}`;
     rounds.innerText = `${round}`;
     computerImg[0].style.filter = 'grayscale(100%)';
+    computerImg[0].style.transform = 'scale(1)';
     computerImg[1].style.filter = 'grayscale(100%)';
+    computerImg[1].style.transform = 'scale(1)';
     computerImg[2].style.filter = 'grayscale(100%)';
+    computerImg[2].style.transform = 'scale(1)';
+
+    playerImg[0].style.filter = 'grayscale(100%)';
+    playerImg[0].style.transform = 'scale(1)';
+    playerImg[1].style.filter = 'grayscale(100%)';
+    playerImg[1].style.transform = 'scale(1)';
+    playerImg[2].style.filter = 'grayscale(100%)';
+    playerImg[2].style.transform = 'scale(1)';
+
+    gameInfo.innerText = 'Select your weapon to play';
+    playAgainButton.style.display = 'none';
+
+    weaponButtons.forEach((button) => {
+        button.disabled = false;
+    });
 }
 
 function endGame(playerWins, computerWins) {
-    if (playerWins === 2) {
+    if (playerWins === 5) {
         gameInfo.innerText = 'Game has ended, congratulations you win!';
         playAgainButton.style.display = "inline-block";
-    } else if (computerWins === 2) {
+        weaponButtons.forEach((button) => {
+            button.disabled = true;
+        });
+    } else if (computerWins === 5) {
         gameInfo.innerText = 'Game has ended, better luck next time!';
         playAgainButton.style.display = "inline-block";
+        weaponButtons.forEach((button) => {
+            button.disabled = true;
+        });
     }
 }
 
 function playGame() {
-    let playerSelection = prompt();
+    let playerSelection = '';
 
-    playRound(playerSelection, getComputerChoice());
-    countRounds();
-    endGame(playerWins, computerWins);
+    weaponButtons.forEach((weapon) => {
+        weapon.addEventListener('click', () => {
+            if(weapon.classList.contains('rock')){
+                playerImg[0].style.filter = 'none';
+                playerImg[0].style.transform = 'scale(1.1)';
+                playerImg[1].style.filter = 'grayscale(100%)';
+                playerImg[1].style.transform = 'scale(1)';
+                playerImg[2].style.filter = 'grayscale(100%)';
+                playerImg[2].style.transform = 'scale(1)';
+                playerSelection = 'Rock';
+                // console.log(playerSelection)
+            }else if(weapon.classList.contains('paper')){
+                playerImg[0].style.filter = 'grayscale(100%)';
+                playerImg[0].style.transform = 'scale(1)';
+                playerImg[1].style.filter = 'none';
+                playerImg[1].style.transform = 'scale(1.1)';
+                playerImg[2].style.filter = 'grayscale(100%)';
+                playerImg[2].style.transform = 'scale(1)';
+                playerSelection = 'Paper';
+                // console.log(playerSelection)
+            }else if(weapon.classList.contains('scissors')){
+                playerImg[0].style.filter = 'grayscale(100%)';
+                playerImg[0].style.transform = 'scale(1)';
+                playerImg[1].style.filter = 'grayscale(100%)';
+                playerImg[1].style.transform = 'scale(1)';
+                playerImg[2].style.filter = 'none';
+                playerImg[2].style.transform = 'scale(1.1)';
+                playerSelection = 'Scissors';
+                // console.log(playerSelection)
+            }
+            countRounds();
+            playRound(playerSelection, getComputerChoice());
+            endGame(playerWins, computerWins);
+        });
+    });
+    
+    playAgainButton.addEventListener('click', () => {
+        resetGame();
+    });
+
+    
 }
+
+playGame();
